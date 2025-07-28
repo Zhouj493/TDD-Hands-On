d0e53e6dda2f9f0de83425f2467c56a2c6b90ba3
Chloe-0115 <114641699+Chloe-0115@users.noreply.github.com>
2025-07-28 15:39:48 -0700

    Create tests.py
    Set up unittest format

---
+++ tests.py
@@ -0,0 +1,14 @@
+# A2: TDD - tests.py
+# Date: 7/28/25
+# Author: Jiayi Zhou
+# Description: This is the unit test file for the check_pwd module, with TDD process.
+
+import unittest
+from check_pwd import check_pwd  #check_pwd is requirement in Canvas explaination
+
+
+class TestCheckPwd(unittest.TestCase):
+  pass
+
+if __name__ == "__main__":
+  unittest.main()
