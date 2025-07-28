ed223a22fe2fa719090b6040d9a402e226e596e4
Chloe-0115 <114641699+Chloe-0115@users.noreply.github.com>
2025-07-28 15:44:48 -0700

    Update check_pwd.py for too short password
    Check if password is too short (less than 8 characters)

--- check_pwd.py
+++ check_pwd.py
@@ -4,5 +4,10 @@
 # Description: This is the check_pwd function. It checks if the password meets requirements.
 
 
+# Citation for the following lines:
+# Adapted from: https://docs.python.org/3/library/stdtypes.html
 def check_pwd(pwd):
-  pass
+    # Check if password is too short (less than 8 characters)
+    if len(pwd) < 8:
+        return False
+    return True
