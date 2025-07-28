bc388cb00a41c142927ae549d312e2f25b23e372
Chloe-0115 <114641699+Chloe-0115@users.noreply.github.com>
2025-07-28 15:40:53 -0700

    Create check_pwd.py
    Set up check_pwd

---
+++ check_pwd.py
@@ -0,0 +1,8 @@
+# A2: TDD - check_pwd.py
+# Date: 7/28/25
+# Author: Jiayi Zhou
+# Description: This is the check_pwd function. It checks if the password meets requirements.
+
+
+def check_pwd(pwd):
+  pass
