66ed8c906f5e150848b17d26321b99ff77c76469
Chloe-0115 <114641699+Chloe-0115@users.noreply.github.com>
2025-07-28 15:52:31 -0700

    Update check_pwd.py for at least one lowercase
    Check for at least one lowercase letter

--- check_pwd.py
+++ check_pwd.py
@@ -6,11 +6,15 @@
 
 # Citation for the following lines:
 # Adapted from: https://docs.python.org/3/library/stdtypes.html
+# And https://www.guru99.com/test-driven-development.html
 def check_pwd(pwd):
     # Check if password is too short (less than 8 characters)
     if len(pwd) < 8:
         return False
     # Check if password is too long (more than 20 characters)
     if len(pwd) > 20:
+        return False
+    # Check for at least one lowercase letter
+    if not any(c.islower() for c in pwd):
         return False
     return True
